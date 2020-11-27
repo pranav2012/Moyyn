79f4a11a2a7e9ef5dbce55860db6de37c8e6cb52
pranav2012 <p2012agarwal@gmail.com>
2020-11-27 15:53:03 +0530

    fixed button size

--- src/Components/Dashboard/JobCard/Jobcard.jsx
+++ src/Components/Dashboard/JobCard/Jobcard.jsx
@@ -15,8 +15,8 @@ function Jobcard() {
             <div style={{borderColor:"rgb(236, 236, 236)"}} className='h-100 flex flex-column w-25-l w-40-m w-30 bl'>
                 <p className='gray f8 f9-m f10-mo mb1 mb3-m mb3-l self-end'>Created 7 hours ago</p>
                 <div className='flex justify-center flex-row-l flex-row-m flex-column justify-center items-center'>
-                    <button style={{background:"#265cff"}} className="ml2-l ml2-m ml3 pointer fw6 f10-mo f8-m f7-l mr2-l mr2-m mr0 w-60-l w-50-m w-80 h2-l h2-m h7-mo bn link dim br2 ph3 pv2 dib white">Candidates</button>
-                    <button style={{color:"#265cff"}} className="bg-white pointer ml2-l ml2-m ml3 shadow-1 fw6 f10-mo f8-m f7-l w-60-l w-50-m w-80 h2-l h2-m h7-mo mt1 mt0-l mt0-m bn link dim br2 ph3 pv2 dib">Edit Job</button>
+                    <button style={{background:"#265cff"}} className="ml2-l ml2-m ml3 pointer fw6 f10-mo f8-m f7-l mr2-l mr2-m mr0 w-60-l w-50-m w-90 h2-l h2-m h7-mo bn link dim br2 ph3 pv2 dib white">Candidates</button>
+                    <button style={{color:"#265cff"}} className="bg-white pointer ml2-l ml2-m ml3 shadow-1 fw6 f10-mo f8-m f7-l w-60-l w-50-m w-90 h2-l h2-m h7-mo mt1 mt0-l mt0-m bn link dim br2 ph3 pv2 dib">Edit Job</button>
                </div>
             </div>
         </div>
