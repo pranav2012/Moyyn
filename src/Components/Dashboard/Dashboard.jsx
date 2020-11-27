17dd349e322c84fe34f6afcb0055779788624596
pranav2012 <p2012agarwal@gmail.com>
2020-11-27 15:01:03 +0530

    added custom css

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import 'tachyons';
 import JobCardList from '../Dashboard/JobCard/Jobcardlist';
 import '../../styles/dashboard.scss';
-//import {PieChart} from 'react-minimal-pie-chart';
+import '../../styles/custom.css';
 
 function Dashboard({company="Moyyn"}) {
     const [jobdata] = useState(['a','b','c','d']);
@@ -12,9 +12,9 @@ function Dashboard({company="Moyyn"}) {
             <div className='flex justify-between items-center'>
                 <div>
                     <p className='mt4 ma0 f3-l f4-m f6'>Welcome to {company} Dashboard!</p>
-                    <p className='mb4-l mb4-m mb3 f5-l f6-m f9 gray ma0'>Get candidates by our A.I system</p>
+                    <p className='mb4-l mb4-m mb3 f5-l f6-m f9-m gray ma0'>Get candidates by our A.I system</p>
                 </div>
-                <button style={{background:"#265cff"}} className="mt3 h2 pointer fw6 f8 f7-m f6-l mr2 w4-l w4-m w7 bn link dim br2 ph3 pv2 dib white">Post a Job</button>
+                <button style={{background:"#265cff"}} className="mt3 h2 pointer fw6 f8-mo f7-m f6-l mr2 w4-l w4-m w7-mo bn link dim br2 ph3 pv2 dib white">Post a Job</button>
             </div>
             <div className="flex ma-4 w-90-l w-90-m w-100 center ">
                 <JobCardList jobs={jobdata}/>
--- src/Components/Dashboard/JobCard/Jobcard.jsx
+++ src/Components/Dashboard/JobCard/Jobcard.jsx
@@ -5,18 +5,18 @@ function Jobcard() {
         <div className="card ma1 bg-white ph3 mt3 br3 flex justify-between items-center">
             <div className='flex flex-column justify-center'>
                 <p className='ma0 f7-l f8-m f10-mo'>Senior level</p>
-                <p className='ma0 f4-l f5-m f8'>Full stack developer</p>
-                <p className='ma0 gray f6-l f8-m f9'>New Delhi, India</p>
+                <p className='ma0 f4-l f5-m f8-mo'>Full stack developer</p>
+                <p className='ma0 gray f6-l f8-m f9-mo'>New Delhi, India</p>
             </div>
             <div className='flex flex-column justify-center items-center'>
-            <p className='ma0 dn db-m db-l f7-l f8-m f9'>No. of Candidates </p>
-            <p className='ma0 gray mt2 f7-l f8-m f9'>0</p>
+            <p className='ma0 dn db-m db-l f7-l f8-m f9-mo'>No. of Candidates </p>
+            <p className='ma0 gray mt2 f7-l f8-m f9-mo'>0</p>
             </div>
             <div style={{borderColor:"rgb(236, 236, 236)"}} className='h-100 flex flex-column w-25-l w-40-m w-30 bl'>
                 <p className='gray f8 f9-m f10-mo mb1 mb3-m mb3-l self-end'>Created 7 hours ago</p>
                 <div className='flex justify-center flex-row-l flex-row-m flex-column justify-center items-center'>
-                    <button style={{background:"#265cff"}} className="ml2-l ml2-m ml3 pointer fw6 f10-mo f8-m f7-l mr2-l mr2-m mr0 w-60-l w-50-m w-80 h2-l h2-m h7 bn link dim br2 ph3 pv2 dib white">Candidates</button>
-                    <button style={{color:"#265cff"}} className="bg-white pointer ml2-l ml2-m ml3 shadow-1 fw6 f10-mo f8-m f7-l w-60-l w-50-m w-80 h2-l h2-m h7 mt1 mt0-l mt0-m bn link dim br2 ph3 pv2 dib">Edit Job</button>
+                    <button style={{background:"#265cff"}} className="ml2-l ml2-m ml3 pointer fw6 f10-mo f8-m f7-l mr2-l mr2-m mr0 w-60-l w-50-m w-80 h2-l h2-m h7-mo bn link dim br2 ph3 pv2 dib white">Candidates</button>
+                    <button style={{color:"#265cff"}} className="bg-white pointer ml2-l ml2-m ml3 shadow-1 fw6 f10-mo f8-m f7-l w-60-l w-50-m w-80 h2-l h2-m h7-mo mt1 mt0-l mt0-m bn link dim br2 ph3 pv2 dib">Edit Job</button>
                </div>
             </div>
         </div>
