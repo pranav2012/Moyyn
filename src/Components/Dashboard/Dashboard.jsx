52df6ed393e0c131881c8e4bd0f779c2ae9857a4
pranav2012 <p2012agarwal@gmail.com>
2020-11-28 15:48:40 +0530

    fixed fonts

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -9,12 +9,12 @@ function Dashboard({company="Moyyn"}) {
     return (
         <div style={{background:"#eef2f5"}} className='flex-1 w-100 vh-100'>
         <div className='w-90 center mb5'>
-            <div className='flex justify-between items-center'>
-                <div>
-                    <p className='mt4 ma0 f3-l f4-m f6'>Welcome to {company} Dashboard!</p>
-                    <p className='mb4-l mb4-m mb3 f5-l f6-m f9-m gray ma0'>Get candidates by our A.I system</p>
+            <div className='flex mt4 mb3 center justify-between w-90 items-center'>
+                <div className='flex flex-column justify-between '>
+                    <p className='ma0 f3-l f4-m f6'>Welcome to {company} Dashboard!</p>
+                    <p className='f5-l f6-m f9-m gray ma0'>Get candidates by our A.I system</p>
                 </div>
-                <button style={{background:"#265cff"}} className="mt3 h2 pointer h7-mo fw6 f9-mo f7-m f6-l mr2 w4-l w4-m w7-mo bn link dim br2 ph3 pv2 dib white">Post a Job</button>
+                <button style={{background:"#265cff"}} className="h2 pointer h7-mo fw6 f8-mo f7-m f6-l mr2 w-20-l w-20-m w4 bn link dim br2 ph3 pv2 dib white">Post a Job</button>
             </div>
             <div className="flex ma-4 w-90-l w-90-m w-100 center ">
                 <JobCardList jobs={jobdata}/>
--- src/Components/Dashboard/Header/DashboardHeader.jsx
+++ src/Components/Dashboard/Header/DashboardHeader.jsx
@@ -8,13 +8,13 @@ function DashboardHeader() {
         <div className='header w-100 h3'>
             <div className='flex justify-start headflex items-center w-90 center'>
                 <div className='logoheader w4'><a className='link' href="https://moyyn.com/"><h3 className='white logo f4'>Moyyn</h3></a></div>
-                <div className={`navbar nav-dash ml-auto ${burgerclicked ? 'nav-active' : ''}`}>
-                    <ul className='flex nav w-90 navlist pa0 ma0 justify-between items-center'>
-                        <li className='list fw5 f7 f6-l'><a className='link white dim dib' href="/Dashboard"><p>Dashboard</p></a></li>
-                        <li className='list fw5 f7 f6-l'><a className='link white dim dib' href="/Settings"><p>Settings</p></a></li>
+                <div className={`w-100 navbar nav-dash ml-auto ${burgerclicked ? 'nav-active' : ''}`}>
+                    <ul className='flex nav w-90 navlist pa0 ma0 items-center'>
+                        <li className='center list fw5 f7 f6-l'><a className='link white dim dib' href="/Dashboard"><p>Dashboard</p></a></li>
+                        <li className='center list fw5 f7 f6-l'><a className='link white dim dib' href="/Settings"><p>Settings</p></a></li>
                     </ul>
                 </div>
-                <div className=' ml4 dicon'><a href="/" className='white'><i className="fa fa-sign-out"></i></a></div>
+                <div className='dicon'><a href="/" className='white'><i className="fa fa-sign-out"></i></a></div>
                 <div className={`burger mb2 ${burgerclicked ? 'toggle' : ''}`} onClick={() => { setburger(burgerclicked = !burgerclicked) }}>
                     <div className='line1'></div>
                     <div className='line2'></div>
