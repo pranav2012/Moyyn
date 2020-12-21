d817fab340e71039438a81e9b6899efe33dce144
Pranav Agarwal <p2012agarwal@gmail.com>
2020-12-21 14:08:50 +0530

    filter icon container width

--- src/Components/Candidates/Filter.jsx
+++ src/Components/Candidates/Filter.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import PublicOutlinedIcon from '@material-ui/icons/PublicOutlined';
 import ScheduleOutlinedIcon from '@material-ui/icons/ScheduleOutlined';
 import WorkOutlineOutlinedIcon from '@material-ui/icons/WorkOutlineOutlined';
@@ -6,13 +6,13 @@ import TranslateOutlinedIcon from '@material-ui/icons/TranslateOutlined';
 import Chip from '@material-ui/core/Chip';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import {countries, industries, languages, notices} from '../../util/data/static-data';
+import { countries, industries, languages, notices } from '../../util/data/static-data';
 
 function Filter() {
-    
+
     const [filteradded] = useState(true);
     // const [filters, setfilters] = useState(null)
-    const [dl,setdl] = useState(false);
+    const [dl, setdl] = useState(false);
     const [countriess] = useState(countries);
     const [industriess] = useState(industries);
     const [languagess] = useState(languages);
@@ -21,7 +21,7 @@ function Filter() {
     const [industry, setindustry] = useState(null);
     const [language, setlanguage] = useState(null);
     const [notice, setnotice] = useState(null);
-    
+
     const [countryvalue, setcountryvalue] = useState("");
     const [industryvalue, setindustryvalue] = useState("");
     const [languagevalue, setlanguagevalue] = useState("");
@@ -66,7 +66,7 @@ function Filter() {
         console.log("e.target")
     };
 
-    const handledelete = () =>{
+    const handledelete = () => {
         console.log("deleted");
     }
 
@@ -81,119 +81,121 @@ function Filter() {
     //         });
     //     }
     // }, [])
-    
+
     return (
         <>
             <div className={''/*"bg-white br2 c-shadow pa2 mt3"*/}>
-            <div className='w-100 center mt3'><p className='ma0 gray pl2 f5-l f5-m f7'>Filter Candidates</p></div>
-            <div style={{color:"#265cff"}} className='candidate_search h3 c-shadow bg-white w-100-l w-90 mt3 pv2 ph4-l ph3-m ph2 flex-wrap items-center center br2 flex justify-between'>
-                <div className='ml0-l ml4-m ml4'>
-                    <div aria-controls="country" aria-haspopup="true" onClick={countryclick} className='pointer dim flex flex-column items-center'>
-                        <PublicOutlinedIcon/> 
-                        <p className='ma0 mt1 gray f8 f9-mo'>Country</p>
+                <div className='w-100 center mt3'><p className='ma0 gray pl2 f5-l f5-m f7'>Filter Candidates</p></div>
+                <div style={{ color: "#265cff" }} className='candidate_search h3 c-shadow bg-white w-100-l w-90 mt3 pv2 ph4-l ph3-m ph2 center br2'>
+                    <div className='flex justify-between flex-wrap items-center center w-80-l w-80-m w-100'>
+                        <div className='ml0-l ml4-m ml4'>
+                            <div aria-controls="country" aria-haspopup="true" onClick={countryclick} className='pointer dim flex flex-column items-center'>
+                                <PublicOutlinedIcon />
+                                <p className='ma0 mt1 gray f8 f9-mo'>Country</p>
+                            </div>
+                            <Menu
+                                id="country"
+                                value={countryvalue}
+                                anchorEl={country}
+                                keepMounted
+                                open={Boolean(country)}
+                                onClose={countryclose}
+                                onChange={handlecountry}
+                            >
+                                {countriess.map((option, id) => (
+                                    <MenuItem key={id} onClick={countryclose}>
+                                        {option.label}
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </div>
+                        <div>
+                            <div aria-controls="notice" aria-haspopup="true" onClick={noticeclick} className='pointer dim flex flex-column items-center'>
+                                <ScheduleOutlinedIcon />
+                                <p className='ma0 mt1 gray f8 f9-mo'>Notice Period</p>
+                            </div>
+                            <Menu
+                                id="notice"
+                                value={noticevalue}
+                                anchorEl={notice}
+                                keepMounted
+                                open={Boolean(notice)}
+                                onClose={noticeclose}
+                                onChange={handlenotice}
+                            >
+                                {noticess.map((option, id) => (
+                                    <MenuItem key={id} onClick={noticeclose}>
+                                        {option.time}
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </div>
+                        <div>
+                            <div aria-controls="industry" aria-haspopup="true" onClick={industryclick} className='pointer dim flex flex-column items-center'>
+                                <WorkOutlineOutlinedIcon />
+                                <p className='ma0 mt1 gray f8 f9-mo'>Industry</p>
+                            </div>
+                            <Menu
+                                id="industry"
+                                value={industryvalue}
+                                anchorEl={industry}
+                                keepMounted
+                                open={Boolean(industry)}
+                                onClose={industryclose}
+                                onChange={handleindustry}
+                            >
+                                {industriess.map((option, id) => (
+                                    <MenuItem key={id} onClick={industryclose}>
+                                        {option}
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </div>
+                        <div>
+                            <div aria-controls="language" aria-haspopup="true" onClick={languageclick} className='pointer dim flex flex-column items-center'>
+                                <TranslateOutlinedIcon />
+                                <p className='ma0 mt1 gray f8 f9-mo'>Languages</p>
+                            </div>
+                            <Menu
+                                id="language"
+                                value={languagevalue}
+                                anchorEl={language}
+                                keepMounted
+                                open={Boolean(language)}
+                                onClose={languageclose}
+                                onChange={handlelanguage}
+                            >
+                                {languagess.map((option, id) => (
+                                    <MenuItem key={id} onClick={languageclose}>
+                                        {option.name}
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </div>
+                        <div>
+                            <div className='flex items-center hide-mo'>
+                                <p className='gray mr1 f7-l f7-m f9-mo'>Eu Driving License</p>
+                                <input onChange={() => setdl(!dl)} value={dl} name="othercountries" type="checkbox" />
+                            </div>
+                        </div>
                     </div>
-                    <Menu
-                        id="country"
-                        value={countryvalue}
-                        anchorEl={country}
-                        keepMounted
-                        open={Boolean(country)}
-                        onClose={countryclose}
-                        onChange={handlecountry}
-                    >
-                        {countriess.map((option,id) => (
-                        <MenuItem key={id} onClick={countryclose}>
-                            {option.label}
-                        </MenuItem>
-                        ))}
-                    </Menu>
                 </div>
-                <div>
-                    <div aria-controls="notice" aria-haspopup="true" onClick={noticeclick} className='pointer dim flex flex-column items-center'>
-                        <ScheduleOutlinedIcon/>
-                        <p className='ma0 mt1 gray f8 f9-mo'>Notice Period</p>
-                    </div>
-                    <Menu
-                        id="notice"
-                        value={noticevalue}
-                        anchorEl={notice}
-                        keepMounted
-                        open={Boolean(notice)}
-                        onClose={noticeclose}
-                        onChange={handlenotice}
-                    >
-                        {noticess.map((option,id) => (
-                        <MenuItem key={id} onClick={noticeclose}>
-                            {option.time}
-                        </MenuItem>
-                        ))}
-                    </Menu>
-                </div>
-                <div>
-                    <div aria-controls="industry" aria-haspopup="true" onClick={industryclick} className='pointer dim flex flex-column items-center'>
-                        <WorkOutlineOutlinedIcon/>
-                        <p className='ma0 mt1 gray f8 f9-mo'>Industry</p>
-                    </div>
-                    <Menu
-                        id="industry"
-                        value={industryvalue}
-                        anchorEl={industry}
-                        keepMounted
-                        open={Boolean(industry)}
-                        onClose={industryclose}
-                        onChange={handleindustry}
-                                    >
-                        {industriess.map((option,id) => (
-                        <MenuItem key={id} onClick={industryclose}>
-                            {option}
-                        </MenuItem>
-                        ))}
-                    </Menu>
-                </div>
-                <div>
-                    <div aria-controls="language" aria-haspopup="true" onClick={languageclick}  className='pointer dim flex flex-column items-center'>
-                        <TranslateOutlinedIcon/>
-                        <p className='ma0 mt1 gray f8 f9-mo'>Languages</p>
-                    </div>
-                    <Menu
-                        id="language"
-                        value={languagevalue}
-                        anchorEl={language}
-                        keepMounted
-                        open={Boolean(language)}
-                        onClose={languageclose}
-                        onChange={handlelanguage}
-                    >
-                        {languagess.map((option,id) => (
-                        <MenuItem key={id} onClick={languageclose}>
-                            {option.name}
-                        </MenuItem>
-                        ))}
-                    </Menu>
-                </div>
-                <div>
-                    <div className='flex items-center hide-mo'> 
-                        <p className='gray mr1 f7-l f7-m f9-mo'>Eu Driving License</p>
-                        <input onChange={()=>setdl(!dl)} value={dl} name="othercountries" type="checkbox"/>
-                    </div>
-                </div>
-            </div>
-            <div className='w-90 center tc mt3'>
-              { 
-                filteradded?
-                <div className='chips'>
-                   {/* {
+                <div className='w-90 center tc mt3'>
+                    {
+                        filteradded ?
+                            <div className='chips'>
+                                {/* {
                        filters.map((name,id)=>
                         )
                    } */}
-                    <Chip variant="outlined" size="small" label={"label"} onDelete={handledelete}/>
-                    <Chip variant="outlined" size="small" label={"label"} onDelete={handledelete}/>
-                    <Chip variant="outlined" size="small" label={"label"} onDelete={handledelete}/>
+                                <Chip variant="outlined" size="small" label={"label"} onDelete={handledelete} />
+                                <Chip variant="outlined" size="small" label={"label"} onDelete={handledelete} />
+                                <Chip variant="outlined" size="small" label={"label"} onDelete={handledelete} />
+                            </div>
+                            :
+                            <p className='ma0 pl2 f6-l f6-m f8 gray'>No filter's added</p>
+                    }
                 </div>
-                :
-                <p className='ma0 pl2 f6-l f6-m f8 gray'>No filter's added</p>
-              }
-            </div>
             </div>
         </>
     )
