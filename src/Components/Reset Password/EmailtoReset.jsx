1fca4e8efec819aa0bd782f4fb662c1854e9d699
Aishwarya Nair <aishwariyanair68@gmail.com>
2021-01-07 23:31:04 +0530

    alert msg on password reset

--- src/Components/Reset Password/EmailtoReset.jsx
+++ src/Components/Reset Password/EmailtoReset.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../styles/setting.scss';
 import { Button } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
 import 'tachyons';
 import TextField from '@material-ui/core/TextField';
 
 export default function EmailtoReset() {
+
+    const [emailfound, setemailfound] = useState(false);
+
     return (
         <div className='w-100' style={{background:"#eef2f5"}}>
             <div className='center br2 mv5 tc bg-white w-60 ma2 pa4'>
-            <p className='gray tc f4'>Provide your email address to send a password reset link</p>
-            <div>
+                <p className='gray tc f4'>Provide your email address to send a password reset link</p>
+                <div>
                 <div className='creds mt3 w-100'>
                     <TextField
                         name="mail"
@@ -19,7 +23,10 @@ export default function EmailtoReset() {
                     />
                 </div>
                 <div className='mt4'>
-                    <Button className='dim' variant="outlined" style={{ borderColor: "#265cff", color: "#265cff" }}>Send Email</Button>
+                    <Button onClick={()=>setemailfound(!emailfound)} className='dim' variant="outlined" style={{ borderColor: "#265cff", color: "#265cff" }}>Send Email</Button>
+                </div>
+                <div className={`${emailfound?'':'hide'} mt4`}>
+                    <Alert severity="success">Email sent, please check your email id to reset your password</Alert>
                 </div>
             </div>
         </div>
--- src/Components/Reset Password/Resetpass.jsx
+++ src/Components/Reset Password/Resetpass.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button } from '@material-ui/core';
 import 'tachyons';
 import TextField from '@material-ui/core/TextField';
 import '../../styles/setting.scss';
+import Alert from '@material-ui/lab/Alert';
 
 export default function Resetpass() {
+
+    const [reseted, setreseted] = useState(false);
+
     return (
         <div className='w-100' style={{background:"#eef2f5"}}>
             <div className='center br2 mv5 tc bg-white w-60 ma2 pa4'>
@@ -12,9 +16,10 @@ export default function Resetpass() {
             <div>
                 <div className='creds mt3 w-100'>
                     <TextField
-                        name="Old"
-                        label="Old Password"
+                        name="token"
+                        label="Reset Token"
                         variant="outlined"
+                        autoComplete="off"
                         className={`w-50-l w-70-m w-80`}
                     />
                 </div>
@@ -22,12 +27,17 @@ export default function Resetpass() {
                     <TextField
                         name="new"
                         label="New Password"
+                        type="password"
                         variant="outlined"
+                        autoComplete="off"
                         className={`w-50-l w-70-m w-80`}
                     />
                 </div>
                 <div className='mt4'>
-                    <Button className='dim' variant="outlined" style={{ borderColor: "#265cff", color: "#265cff" }}>Update Password</Button>
+                    <Button onClick={()=>setreseted(!reseted)} className='dim' variant="outlined" style={{ borderColor: "#265cff", color: "#265cff" }}>Update Password</Button>
+                </div>
+                <div className={`${reseted?'':'hide'} mt4`}>
+                    <Alert severity="success">Password reset Succesfull!</Alert>
                 </div>
             </div>
         </div>
